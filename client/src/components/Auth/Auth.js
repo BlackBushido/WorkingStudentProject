60c3296987b5f204fcb093c2ecceeadee6a879f1
BlackBushido <alissa4@wp.pl>
2022-05-24 11:20:01 +0200

    jsonwebtoken and bcrypt are not working

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,43 +1,65 @@
 import React, {useState} from 'react';
 import {Avatar, Button, Paper, Grid, Typography, Container} from "@material-ui/core";
-import { useGoogleLogin } from '@react-oauth/google';
-
+import { useGoogleLogin, GoogleLogin } from '@react-oauth/google';
+import {useDispatch} from "react-redux";
+import {useNavigate} from 'react-router-dom';
 import Icon from './icon';
 import useStyles from './styles';
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Input from './Input';
+import {signIn, signUp} from '../../actions/auth'
+
+const initialState = {firstName: '', lastName: '', email: '', password: '', confirmPassword: ''}
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignUp, setIsSignUp] = useState(false);
+    const [formData, setFormData] = useState(initialState)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
+        if(isSignUp){
+            dispatch(signUp(formData, navigate))
+        } else {
+            dispatch(signIn(formData, navigate))
+        }
     };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
     };
 
     const switchMode = () => {
         setIsSignUp((prevIsSignUp) => !prevIsSignUp);
-        handleShowPassword(false);
+        setShowPassword(false);
     };
 
     const googleFailure = (error) => {
         console.log(error);
         console.log("Google Sign In was unsuccessful. Try Again Later");
     };
-    // const googleSuccess =  async (res) => {
-    //     console.log(res);
-    // };
+    const googleSuccess = (res) => {
+        const result = res?.profileObj;
+        const token = res?.access_token;
+
+        try{
+            dispatch({type: 'AUTH', data: {result, token}});
+
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     const login = useGoogleLogin({
-        onSuccess:   tokenResponse => console.log(tokenResponse),
+        onSuccess:  googleSuccess,
         onError: googleFailure,
 
     });
@@ -57,7 +79,7 @@ const Auth = () => {
                             isSignUp && (
                                 <>
                                     <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half></Input>
-                                    <Input name="firstName" label="First Name" handleChange={handleChange} half></Input>
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half></Input>
                                 </>
                             )
                         }
@@ -71,6 +93,7 @@ const Auth = () => {
                     <Button className={classes.googleButton} color='primary' fullWidth onClick={ () => login()} startIcon={<Icon />} variant="contained">
                         Google Sign In
                     </Button>
+                    {/*<GoogleLogin onSuccess={googleSuccess} />*/}
                     <Grid container justifyContent="flex-end">
                         <Grid item>
                             <Button onClick={switchMode}>
